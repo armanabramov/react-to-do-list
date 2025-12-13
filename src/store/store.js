75f757e1bb635f1798e2336aae5719d64722b17a
armanabramov <128611537+armanabramov@users.noreply.github.com>
2025-12-13 20:53:45 +0300

    chore: enable redux devtools in store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import { todoReducer } from './todos';
 import { searchReducer } from './search';
@@ -10,4 +10,6 @@ const reducer = combineReducers({
 	sort: sortReducer,
 });
 
-export const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
